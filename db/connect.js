065fc874aec16446b2a045a1f15d21922025aaf4
Sergii Golovko <golovko.sergey@gmail.com>
2023-11-06 16:18:49 +0000

    seeding db added

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,16 @@
-const Db = 'mongodb://localhost:27017/flash-cards-db';
+let Db;
 const mongoose = require('mongoose');
+const ENV = process.env.NODE_ENV || 'development';
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+if (!process.env.MONGO_URI || !process.env.MONGO_DB) {
+  throw new Error('MONGO_URI or MONGO_DB not set');
+}
+
+Db = process.env.MONGO_URI + process.env.MONGO_DB;
 
 const connection = async () => {
   await mongoose.connect(Db);
