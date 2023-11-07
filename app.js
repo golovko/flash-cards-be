7aff7816446ed790ca7d7ad6ed0c8c7849482a39
Andrii <andrewtet2012@gmail.com>
2023-11-07 10:31:33 +0000

    added tests, still have and async issue to resolve

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const Card = require('./models/Card');
+const Card = require("./models/Card");
 
-const db_connect = require('./db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const db_connect = require("./db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 app.use(cors());
 app.use(express.json());
 db_connect();
 
-app.get('/api/cards', async function (req, res) {
+app.get("/api/cards", async function (req, res) {
   try {
     const fetchedCards = await Card.find({});
     res.status(200).send(fetchedCards);
@@ -19,7 +19,7 @@ app.get('/api/cards', async function (req, res) {
   }
 });
 
-app.get('/api/cards/:card_id', async function (req, res) {
+app.get("/api/cards/:card_id", async function (req, res) {
   let id = req.params.card_id;
   console.log(id);
   try {
--- db/seed.js
+++ db/seed.js
@@ -1,36 +1,35 @@
-const { biologyCards } = require('./data/cards-mock');
-const MongoClient = require('mongodb').MongoClient;
+const { biologyCards } = require("./data/cards-mock");
+const MongoClient = require("mongodb").MongoClient;
 let uri;
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.MONGO_URI || !process.env.MONGO_DB) {
-  throw new Error('MONGO_URI or MONGO_DB not set');
+  throw new Error("MONGO_URI or MONGO_DB not set");
 }
 
 uri = process.env.MONGO_URI;
 
 async function seedDB() {
-  const client = new MongoClient(uri);
+  const client = await new MongoClient(uri);
 
   try {
     await client.connect();
-    console.log('Connected correctly to server');
-    const collection = client.db(process.env.MONGO_DB).collection('cards');
-    collection.drop();
-    collection
-      .insertMany(biologyCards)
-      .then((val) => {
-        console.log(val.insertedIds);
-        console.log('Database seeded! :)');
-      })
-      .finally(() => client.close());
+    console.log("Connected correctly to server");
+    const collection = await client
+      .db(process.env.MONGO_DB)
+      .collection("cards");
+    await collection.drop();
+    await collection.insertMany(biologyCards);
+    await client.close();
   } catch (err) {
     console.log(err.stack);
   }
 }
 
 seedDB();
+
+module.exports = seedDB;
