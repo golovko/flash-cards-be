22836a6ff0c588a2fdbce0d4b37e25d884a81954
Sergii Golovko <golovko.sergey@gmail.com>
2023-11-08 15:15:33 +0000

    re-deploy

--- models/cards.model.js
+++ models/cards.model.js
@@ -1,7 +1,6 @@
 const db = require('../db/newConnect');
 
 module.exports.cardsFetch = async () => {
-  
   try {
     await db.connect();
     const collection = db.getCollection('cards');
@@ -14,25 +13,21 @@ module.exports.cardsFetch = async () => {
   }
 };
 
-
 module.exports.insertCard = async (newCard) => {
-  const { question, answer, topic} = newCard;
- 
+  const { question, answer, topic } = newCard;
+
   try {
     await db.connect();
     const collection = db.getCollection('cards');
-    const insertedCard =  await collection.insertOne({
-        question,
-        answer,
-        topic,
-      });
-      return insertedCard;
-  
-
+    const insertedCard = await collection.insertOne({
+      question,
+      answer,
+      topic,
+    });
+    return insertedCard;
   } catch (err) {
     throw err;
   } finally {
     await db.close();
   }
 };
-      
