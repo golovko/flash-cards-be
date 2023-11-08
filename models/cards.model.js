fc801938dfaec0cfefd24dd3a280d49f5908a7d0
Sergii Golovko <golovko.sergey@gmail.com>
2023-11-08 09:37:30 +0000

    mvc pattern implemented

---
+++ models/cards.model.js
@@ -0,0 +1,14 @@
+const db = require('../db/newConnect');
+
+module.exports.cardsFetch = async () => {
+  try {
+    await db.connect();
+    const collection = db.getCollection('cards');
+    const fetchedCards = await collection.find({}).toArray();
+    return fetchedCards;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    db.close();
+  }
+};
